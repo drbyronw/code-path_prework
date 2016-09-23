0f8971c98d5b38437bb906245dfac8b27d212e00
Byron J. Williams <byron.w@ieee.org>
2016-09-22 21:23:21 -0500

    Initial Commit

---
+++ tipper2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipper2
+//
+//  Created by Byron J. Williams on 9/22/16.
+//  Copyright © 2016 Byron J. Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
